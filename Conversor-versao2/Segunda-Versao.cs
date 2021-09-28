94a28960ac0347ff987a5e50e4e5258b05d85134
Rômulo Queiroz <rfcontatosvia@gmail.com>
2021-09-28 12:22:55 -0300

    Segunda-versao

---
+++ Conversor-versao2/Segunda-Versao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conversor.de.Moeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double valor, valorConvertido;
+            int opcao;
+
+
+            Console.WriteLine("Digite o valor a ser convertido; ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Opcao 1 -Converter de real para dola, Opcao 2-Converter de dolar para real");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao <= 1)
+            {
+                valorConvertido = valor / 5.39;
+            }
+            else
+            {
+                valorConvertido = valor * 5.39;
+            }
+
+            Console.WriteLine("Valor convertido: " + valorConvertido.ToString("F2", CI));
+            //Esse será a segunda Versão
